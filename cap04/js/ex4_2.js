2c4cd6a727a57fd6b8ec22d5ef20a8f9cba7813c
gclemos <gabriel.clemos@hotmail.com>
2024-03-01 15:34:02 -0300

    Ideal Weight Calculator Program. Using conditional statments combined with a radio button.

---
+++ cap04/js/ex4_2.js
@@ -0,0 +1,23 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const name = frm.inName.value
+    const masculine = frm.inMasculine.value
+    const height = frm.inHeight.value
+
+    let weight
+    if (masculine) {
+        weight = 22 * Math.pow(height, 2)
+    } else {
+        weight = 21 * Math.pow(height, 2)
+    }
+
+    resp.innerText = `${name}: Your ideal weight is ${weight.toFixed(3)} kg`
+})
+
+frm.addEventListener("reset", () => {
+    resp.innerText = ""
+})
